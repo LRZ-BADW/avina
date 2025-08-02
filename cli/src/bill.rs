a4011a1a3654f103ccf259b32b35fbdb8df1df22
Sandro-Alessio Gierens <sandro@gierens.de>
2025-08-02 16:30:26 +0200

    feat(cli): add rudimentary saving of bill pdf
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- cli/src/bill.rs
+++ cli/src/bill.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fs::write};
 
 use crate::common::{Format, print_single_object};
 
@@ -6,5 +6,7 @@ pub(crate) async fn bill_get(
     api: avina::Api,
     format: Format,
 ) -> Result<(), Box<dyn Error>> {
-    print_single_object(&api.bill.get().await?, format)
+    let bill = api.bill.get().await?;
+    write("bill.pdf", bill.pdf.clone())?;
+    print_single_object(&bill, format)
 }
