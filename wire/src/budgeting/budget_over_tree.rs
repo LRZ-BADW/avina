6bc5cc5fd0a2269dfba8845152cc484520e03c42
Sandro-Alessio Gierens <sandro@gierens.de>
2025-08-13 15:26:41 +0200

    feat(lib,wire): add BudgetOverTreeParams
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- wire/src/budgeting/budget_over_tree.rs
+++ wire/src/budgeting/budget_over_tree.rs
@@ -1,6 +1,15 @@
 use std::collections::HashMap;
 
+use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BudgetOverTreeParams {
+    pub all: Option<bool>,
+    pub project: Option<u32>,
+    pub user: Option<u32>,
+    pub end: Option<DateTime<FixedOffset>>,
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct BudgetOverTreeServer {
