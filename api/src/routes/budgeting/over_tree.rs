625e5e06ed99db117761c74ddf3b10035e2eeed4
Sandro-Alessio Gierens <sandro@gierens.de>
2025-08-13 15:46:06 +0200

    feat(api/routes): add bare budget_over_for_user/project/all functions
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/budgeting/over_tree.rs
+++ api/src/routes/budgeting/over_tree.rs
@@ -4,11 +4,11 @@ use actix_web::{
 };
 use anyhow::Context;
 use avina_wire::{
-    budgeting::{BudgetOverTree, BudgetOverTreeParams},
+    budgeting::{BudgetOverTree, BudgetOverTreeParams, BudgetOverTreeUser},
     user::User,
 };
-use chrono::Utc;
-use sqlx::MySqlPool;
+use chrono::{DateTime, Utc};
+use sqlx::{MySql, MySqlPool, Transaction};
 
 use crate::{
     authorization::{
@@ -16,8 +16,31 @@ use crate::{
         require_user_or_project_master_or_not_found,
     },
     database::user::user::select_user_from_db,
-    error::OptionApiError,
+    error::{OptionApiError, UnexpectedOnlyError},
 };
+
+async fn budget_over_tree_for_user(
+    transaction: &mut Transaction<'_, MySql>,
+    user_id: u64,
+    end: DateTime<Utc>,
+) -> Result<BudgetOverTree, UnexpectedOnlyError> {
+    todo!()
+}
+
+async fn budget_over_tree_for_project(
+    transaction: &mut Transaction<'_, MySql>,
+    project_id: u64,
+    end: DateTime<Utc>,
+) -> Result<BudgetOverTree, UnexpectedOnlyError> {
+    todo!()
+}
+
+async fn budget_over_tree_for_all(
+    transaction: &mut Transaction<'_, MySql>,
+    end: DateTime<Utc>,
+) -> Result<BudgetOverTree, UnexpectedOnlyError> {
+    todo!()
+}
 
 #[tracing::instrument(name = "budget_over_tree")]
 pub async fn budget_over_tree(
@@ -31,12 +54,17 @@ pub async fn budget_over_tree(
         .begin()
         .await
         .context("Failed to begin transaction")?;
-    let over: BudgetOverTree = if params.all.unwrap_or(false) {
+    let over = if params.all.unwrap_or(false) {
         require_admin_user(&user)?;
-        todo!();
+        budget_over_tree_for_all(&mut transaction, end.into()).await?
     } else if let Some(project_id) = params.project {
         require_master_user_or_return_not_found(&user, project_id)?;
-        todo!();
+        budget_over_tree_for_project(
+            &mut transaction,
+            project_id as u64,
+            end.into(),
+        )
+        .await?
     } else if let Some(user_id) = params.user {
         let user_queried =
             select_user_from_db(&mut transaction, user_id as u64).await?;
@@ -45,9 +73,11 @@ pub async fn budget_over_tree(
             user_id,
             user_queried.project,
         )?;
-        todo!();
+        budget_over_tree_for_user(&mut transaction, user_id as u64, end.into())
+            .await?
     } else {
-        todo!();
+        budget_over_tree_for_user(&mut transaction, user.id as u64, end.into())
+            .await?
     };
     transaction
         .commit()
