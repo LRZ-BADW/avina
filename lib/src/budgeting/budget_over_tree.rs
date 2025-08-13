f1a047c9c67ab5d4784b1f051b14b410e81b2668
Sandro-Alessio Gierens <sandro@gierens.de>
2025-08-13 15:26:41 +0200

    feat(lib,wire): add BudgetOverTreeParams
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- lib/src/budgeting/budget_over_tree.rs
+++ lib/src/budgeting/budget_over_tree.rs
@@ -1,9 +1,9 @@
 use std::rc::Rc;
 
 use anyhow::Context;
-use avina_wire::budgeting::BudgetOverTree;
+use avina_wire::budgeting::{BudgetOverTree, BudgetOverTreeParams};
 use chrono::{DateTime, FixedOffset};
-use reqwest::{Client, Method, StatusCode, Url};
+use reqwest::{Client, Method, StatusCode};
 
 use crate::{
     common::{SerializableNone, request},
@@ -21,10 +21,7 @@ pub struct BudgetOverTreeRequest {
     url: String,
     client: Rc<Client>,
 
-    all: bool,
-    project: Option<u32>,
-    user: Option<u32>,
-    end: Option<DateTime<FixedOffset>>,
+    params: BudgetOverTreeParams,
 }
 
 impl BudgetOverTreeRequest {
@@ -32,31 +29,24 @@ impl BudgetOverTreeRequest {
         Self {
             url: url.to_string(),
             client: Rc::clone(client),
-            all: false,
-            project: None,
-            user: None,
-            end: None,
-        }
-    }
 
-    fn params(&self) -> Vec<(&str, String)> {
-        let mut params = Vec::new();
-        if self.all {
-            params.push(("all", "1".to_string()));
-        } else if let Some(project) = self.project {
-            params.push(("project", project.to_string()));
-        } else if let Some(user) = self.user {
-            params.push(("user", user.to_string()));
+            params: BudgetOverTreeParams {
+                all: None,
+                project: None,
+                user: None,
+                end: None,
+            },
         }
-        if let Some(end) = self.end {
-            params.push(("end", end.to_string()));
-        }
-        params
     }
 
     pub async fn send(&self) -> Result<BudgetOverTree, ApiError> {
-        let url = Url::parse_with_params(self.url.as_str(), self.params())
-            .context("Could not parse URL GET parameters.")?;
+        let params = serde_urlencoded::to_string(&self.params)
+            .context("Failed to encode URL parameters")?;
+        let url = if params.is_empty() {
+            self.url.clone()
+        } else {
+            format!("{}?{}", self.url, params)
+        };
         request(
             &self.client,
             Method::GET,
@@ -68,22 +58,22 @@ impl BudgetOverTreeRequest {
     }
 
     pub fn all(&mut self) -> &mut Self {
-        self.all = true;
+        self.params.all = Some(true);
         self
     }
 
     pub fn project(&mut self, project: u32) -> &mut Self {
-        self.project = Some(project);
+        self.params.project = Some(project);
         self
     }
 
     pub fn user(&mut self, user: u32) -> &mut Self {
-        self.user = Some(user);
+        self.params.user = Some(user);
         self
     }
 
     pub fn end(&mut self, end: DateTime<FixedOffset>) -> &mut Self {
-        self.end = Some(end);
+        self.params.end = Some(end);
         self
     }
 }
