3261ece0c2f00e24e1e01631c2ad61589dbf98b9
Sandro-Alessio Gierens <sandro@gierens.de>
2025-08-19 00:00:03 +0200

    feat(api/budgeting): implement budget_over_tree_for_user function
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/budgeting/over_tree.rs
+++ api/src/routes/budgeting/over_tree.rs
@@ -4,27 +4,128 @@ use actix_web::{
 };
 use anyhow::Context;
 use avina_wire::{
-    budgeting::{BudgetOverTree, BudgetOverTreeParams, BudgetOverTreeUser},
+    budgeting::{
+        BudgetOverTree, BudgetOverTreeParams, BudgetOverTreeProject,
+        BudgetOverTreeServer, BudgetOverTreeUser,
+    },
     user::User,
 };
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Datelike, Utc};
 use sqlx::{MySql, MySqlPool, Transaction};
+
+use std::collections::HashMap;
 
 use crate::{
     authorization::{
         require_admin_user, require_master_user_or_return_not_found,
         require_user_or_project_master_or_not_found,
     },
-    database::user::user::select_user_from_db,
-    error::{OptionApiError, UnexpectedOnlyError},
+    database::{
+        budgeting::{
+            project_budget::select_maybe_project_budget_by_project_and_year_from_db,
+            user_budget::select_maybe_user_budget_by_user_and_year_from_db,
+        },
+        user::user::select_user_from_db,
+    },
+    error::{
+        NotFoundOrUnexpectedApiError, OptionApiError, UnexpectedOnlyError,
+    },
+    routes::server_cost::get::calculate_server_cost_for_project_detail,
+    utils::start_of_the_year,
 };
 
 async fn budget_over_tree_for_user(
     transaction: &mut Transaction<'_, MySql>,
     user_id: u64,
     end: DateTime<Utc>,
-) -> Result<BudgetOverTree, UnexpectedOnlyError> {
-    todo!()
+) -> Result<BudgetOverTree, NotFoundOrUnexpectedApiError> {
+    let mut tree = BudgetOverTree {
+        cost: None,
+        projects: HashMap::new(),
+        flavors: None,
+    };
+    let year = end.year();
+    let begin = start_of_the_year(year as u32);
+    let user = select_user_from_db(transaction, user_id).await?;
+    let project_budget =
+        select_maybe_project_budget_by_project_and_year_from_db(
+            transaction,
+            user.project as u64,
+            year as u32,
+        )
+        .await?;
+    let user_budget = select_maybe_user_budget_by_user_and_year_from_db(
+        transaction,
+        user.id as u64,
+        year as u32,
+    )
+    .await?;
+    let project_cost = calculate_server_cost_for_project_detail(
+        transaction,
+        user.project as u64,
+        begin,
+        end,
+    )
+    .await?;
+
+    tree.cost = Some(project_cost.total);
+    tree.projects.insert(
+        user.project_name.clone(),
+        BudgetOverTreeProject {
+            cost: project_cost.total,
+            budget_id: None,
+            budget: None,
+            over: false,
+            users: HashMap::new(),
+            flavors: Some(project_cost.flavors),
+        },
+    );
+    let tree_project = tree.projects.get_mut(&user.project_name).unwrap();
+
+    if let Some(project_budget) = project_budget {
+        tree_project.budget_id = Some(project_budget.id);
+        tree_project.budget = Some(project_budget.amount as u64);
+        tree_project.over = project_cost.total >= project_budget.amount as f64;
+    }
+
+    for (username, user_cost) in project_cost.users {
+        if username != user.name {
+            continue;
+        }
+
+        tree_project.users.insert(
+            user.name.clone(),
+            BudgetOverTreeUser {
+                cost: user_cost.total,
+                budget_id: None,
+                budget: None,
+                over: false,
+                servers: HashMap::new(),
+                flavors: user_cost.flavors,
+            },
+        );
+        let tree_user = tree_project.users.get_mut(&user.name).unwrap();
+
+        if let Some(user_budget) = user_budget {
+            tree_user.budget_id = Some(user_budget.id);
+            tree_user.budget = Some(user_budget.amount as u64);
+            tree_user.over = user_cost.total >= user_budget.amount as f64;
+        }
+
+        for (server_uuid, server_cost) in user_cost.servers {
+            tree_user.servers.insert(
+                server_uuid,
+                BudgetOverTreeServer {
+                    total: server_cost.total,
+                    flavors: server_cost.flavors,
+                },
+            );
+        }
+
+        break;
+    }
+
+    Ok(tree)
 }
 
 async fn budget_over_tree_for_project(
