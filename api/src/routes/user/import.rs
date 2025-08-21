41d80916117fd4d9b0283712c8af2f0a8b5e20de
Sandro-Alessio Gierens <sandro@gierens.de>
2025-08-21 19:32:28 +0200

    feat(api/user): only allow admin to use user_import
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/import.rs
+++ api/src/routes/user/import.rs
@@ -1,7 +1,12 @@
 use actix_web::{HttpResponse, web::ReqData};
 use avina_wire::user::User;
 
+use crate::{authorization::require_admin_user, error::NormalApiError};
+
 #[tracing::instrument(name = "user_import")]
-pub async fn user_import(user: ReqData<User>) -> HttpResponse {
+pub async fn user_import(
+    user: ReqData<User>,
+) -> Result<HttpResponse, NormalApiError> {
+    require_admin_user(&user)?;
     todo!()
 }
