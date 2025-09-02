60c15ec98de2547b8056321af011378dfe6b8327
Sandro-Alessio Gierens <sandro@gierens.de>
2025-09-02 23:31:45 +0200

    feat(api/resources): add bare flavor_group_usage endpoint
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

---
+++ api/src/routes/resources/flavor_group/usage.rs
@@ -0,0 +1,19 @@
+use actix_web::{
+    HttpResponse,
+    web::{Data, Query, ReqData},
+};
+use avina_wire::{resources::FlavorGroupUsageParams, user::User};
+use sqlx::MySqlPool;
+
+use crate::{error::NormalApiError, openstack::OpenStack};
+
+#[tracing::instrument(name = "flavor_usage", skip(_openstack))]
+pub async fn flavor_group_usage(
+    user: ReqData<User>,
+    db_pool: Data<MySqlPool>,
+    _openstack: Data<OpenStack>,
+    params: Query<FlavorGroupUsageParams>,
+    // TODO: is the ValidationError variant ever used?
+) -> Result<HttpResponse, NormalApiError> {
+    todo!()
+}
