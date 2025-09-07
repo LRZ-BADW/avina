1d6e4acb6db8096b39ca40caae338c1d37653940
fredberto <fritz.stelzer@lrz.de>
2025-09-07 10:23:14 +0200

    feat(api/database/pricing): extend flavor_price_list
    close #409

--- api/src/routes/pricing/flavor_price/list.rs
+++ api/src/routes/pricing/flavor_price/list.rs
@@ -1,13 +1,21 @@
 use actix_web::{
     HttpResponse,
-    web::{Data, ReqData},
+    web::{Data, Query, ReqData},
 };
 use anyhow::Context;
-use avina_wire::user::{Project, User};
+use avina_wire::{
+    pricing::FlavorPriceListParams,
+    user::{Project, User},
+};
+use chrono::Utc;
 use sqlx::MySqlPool;
 
 use crate::{
-    database::pricing::flavor_price::select_all_flavor_prices_from_db,
+    database::pricing::flavor_price::{
+        select_all_flavor_prices_from_db,
+        select_flavor_prices_for_period_from_db,
+        select_flavor_prices_for_userclass_from_db,
+    },
     error::NormalApiError,
 };
 
@@ -16,13 +24,24 @@ pub async fn flavor_price_list(
     user: ReqData<User>,
     project: ReqData<Project>,
     db_pool: Data<MySqlPool>,
+    params: Query<FlavorPriceListParams>,
 ) -> Result<HttpResponse, NormalApiError> {
     let mut transaction = db_pool
         .begin()
         .await
         .context("Failed to begin transaction")?;
-    let flavor_prices =
-        select_all_flavor_prices_from_db(&mut transaction).await?;
+
+    let flavor_prices = if params.current.unwrap_or(false) {
+        let now = Utc::now();
+        select_flavor_prices_for_period_from_db(&mut transaction, now, now)
+            .await?
+    } else if let Some(user_class) = params.user_class {
+        select_flavor_prices_for_userclass_from_db(&mut transaction, user_class)
+            .await?
+    } else {
+        select_all_flavor_prices_from_db(&mut transaction).await?
+    };
+
     transaction
         .commit()
         .await
