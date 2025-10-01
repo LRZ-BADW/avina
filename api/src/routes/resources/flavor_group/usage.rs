2d1a7d1024e3a34c6ee7d2eb9110bbab91ab8f57
Sandro-Alessio Gierens <sandro@gierens.de>
2025-10-01 10:53:39 +0200

    feat(api): add function structure for flavor-group usage endpoint
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/resources/flavor_group/usage.rs
+++ api/src/routes/resources/flavor_group/usage.rs
@@ -2,18 +2,214 @@ use actix_web::{
     HttpResponse,
     web::{Data, Query, ReqData},
 };
-use avina_wire::{resources::FlavorGroupUsageParams, user::User};
-use sqlx::MySqlPool;
+use anyhow::Context;
+use avina_wire::{
+    resources::{
+        FlavorGroupUsageAggregate, FlavorGroupUsageParams,
+        FlavorGroupUsageSimple,
+    },
+    user::User,
+};
+use serde::Serialize;
+use sqlx::{MySql, MySqlPool, Transaction};
 
-use crate::{error::NormalApiError, openstack::OpenStack};
+use crate::{
+    authorization::{
+        require_admin_user, require_master_user_or_return_not_found,
+        require_user_or_project_master_or_not_found,
+    },
+    database::user::user::select_user_from_db,
+    error::{OptionApiError, UnexpectedOnlyError},
+    openstack::OpenStack,
+};
 
-#[tracing::instrument(name = "flavor_group_usage", skip(_openstack))]
+#[derive(Serialize)]
+#[serde(untagged)]
+pub enum FlavorGroupUsage {
+    Simple(Vec<FlavorGroupUsageSimple>),
+    Aggregate(Vec<FlavorGroupUsageAggregate>),
+}
+
+pub async fn calculate_flavor_group_usage_for_user_simple(
+    _transaction: &mut Transaction<'_, MySql>,
+    _openstack: Data<OpenStack>,
+    _user_id: u64,
+) -> Result<Vec<FlavorGroupUsageSimple>, UnexpectedOnlyError> {
+    todo!()
+}
+
+pub async fn calculate_flavor_group_usage_for_user_aggregate(
+    _transaction: &mut Transaction<'_, MySql>,
+    _openstack: Data<OpenStack>,
+    _user_id: u64,
+) -> Result<Vec<FlavorGroupUsageAggregate>, UnexpectedOnlyError> {
+    todo!()
+}
+
+pub async fn calculate_flavor_group_usage_for_user(
+    transaction: &mut Transaction<'_, MySql>,
+    openstack: Data<OpenStack>,
+    user_id: u64,
+    aggregate: bool,
+) -> Result<FlavorGroupUsage, UnexpectedOnlyError> {
+    Ok(if aggregate {
+        FlavorGroupUsage::Aggregate(
+            calculate_flavor_group_usage_for_user_aggregate(
+                transaction,
+                openstack,
+                user_id,
+            )
+            .await?,
+        )
+    } else {
+        FlavorGroupUsage::Simple(
+            calculate_flavor_group_usage_for_user_simple(
+                transaction,
+                openstack,
+                user_id,
+            )
+            .await?,
+        )
+    })
+}
+
+pub async fn calculate_flavor_group_usage_for_project_simple(
+    _transaction: &mut Transaction<'_, MySql>,
+    _openstack: Data<OpenStack>,
+    _project_id: u64,
+) -> Result<Vec<FlavorGroupUsageSimple>, UnexpectedOnlyError> {
+    todo!()
+}
+
+pub async fn calculate_flavor_group_usage_for_project_aggregate(
+    _transaction: &mut Transaction<'_, MySql>,
+    _openstack: Data<OpenStack>,
+    _project_id: u64,
+) -> Result<Vec<FlavorGroupUsageAggregate>, UnexpectedOnlyError> {
+    todo!()
+}
+
+pub async fn calculate_flavor_group_usage_for_project(
+    transaction: &mut Transaction<'_, MySql>,
+    openstack: Data<OpenStack>,
+    project_id: u64,
+    aggregate: bool,
+) -> Result<FlavorGroupUsage, UnexpectedOnlyError> {
+    Ok(if aggregate {
+        FlavorGroupUsage::Aggregate(
+            calculate_flavor_group_usage_for_project_aggregate(
+                transaction,
+                openstack,
+                project_id,
+            )
+            .await?,
+        )
+    } else {
+        FlavorGroupUsage::Simple(
+            calculate_flavor_group_usage_for_project_simple(
+                transaction,
+                openstack,
+                project_id,
+            )
+            .await?,
+        )
+    })
+}
+
+pub async fn calculate_flavor_group_usage_for_all_simple(
+    _transaction: &mut Transaction<'_, MySql>,
+    _openstack: Data<OpenStack>,
+) -> Result<Vec<FlavorGroupUsageSimple>, UnexpectedOnlyError> {
+    todo!()
+}
+
+pub async fn calculate_flavor_group_usage_for_all_aggregate(
+    _transaction: &mut Transaction<'_, MySql>,
+    _openstack: Data<OpenStack>,
+) -> Result<Vec<FlavorGroupUsageAggregate>, UnexpectedOnlyError> {
+    todo!()
+}
+
+pub async fn calculate_flavor_group_usage_for_all(
+    transaction: &mut Transaction<'_, MySql>,
+    openstack: Data<OpenStack>,
+    aggregate: bool,
+) -> Result<FlavorGroupUsage, UnexpectedOnlyError> {
+    Ok(if aggregate {
+        FlavorGroupUsage::Aggregate(
+            calculate_flavor_group_usage_for_all_aggregate(
+                transaction,
+                openstack,
+            )
+            .await?,
+        )
+    } else {
+        FlavorGroupUsage::Simple(
+            calculate_flavor_group_usage_for_all_simple(transaction, openstack)
+                .await?,
+        )
+    })
+}
+
+#[tracing::instrument(name = "flavor_group_usage", skip(openstack))]
 pub async fn flavor_group_usage(
     user: ReqData<User>,
     db_pool: Data<MySqlPool>,
-    _openstack: Data<OpenStack>,
+    openstack: Data<OpenStack>,
     params: Query<FlavorGroupUsageParams>,
     // TODO: is the ValidationError variant ever used?
-) -> Result<HttpResponse, NormalApiError> {
-    todo!()
+) -> Result<HttpResponse, OptionApiError> {
+    let aggregate = params.aggregate.unwrap_or(false);
+    let mut transaction = db_pool
+        .begin()
+        .await
+        .context("Failed to begin transaction")?;
+    let usage = if params.all.unwrap_or(false) {
+        require_admin_user(&user)?;
+        calculate_flavor_group_usage_for_all(
+            &mut transaction,
+            openstack,
+            aggregate,
+        )
+        .await?
+    } else if let Some(project_id) = params.project {
+        require_master_user_or_return_not_found(&user, project_id)?;
+        calculate_flavor_group_usage_for_project(
+            &mut transaction,
+            openstack,
+            project_id.into(),
+            aggregate,
+        )
+        .await?
+    } else if let Some(user_id) = params.user {
+        let user_queried =
+            select_user_from_db(&mut transaction, user_id as u64).await?;
+        require_user_or_project_master_or_not_found(
+            &user,
+            user_id,
+            user_queried.project,
+        )?;
+        calculate_flavor_group_usage_for_user(
+            &mut transaction,
+            openstack,
+            user_id.into(),
+            aggregate,
+        )
+        .await?
+    } else {
+        calculate_flavor_group_usage_for_user(
+            &mut transaction,
+            openstack,
+            user.id.into(),
+            aggregate,
+        )
+        .await?
+    };
+    transaction
+        .commit()
+        .await
+        .context("Failed to commit transaction")?;
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .json(usage))
 }
