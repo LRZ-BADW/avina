3bde7dcc2b89cba3370656615f86cf77f5ebfb7d
Sandro-Alessio Gierens <sandro@gierens.de>
2025-10-15 17:38:53 +0200

    feat(api/database): add select_flavor_quota_by_user_and_group_from_db
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/database/quota/flavor_quota.rs
+++ api/src/database/quota/flavor_quota.rs
@@ -180,6 +180,70 @@ pub async fn select_flavor_quotas_by_user_from_db(
 }
 
 #[tracing::instrument(
+    name = "select_maybe_flavor_quota_by_user_and_group_from_db",
+    skip(transaction)
+)]
+pub async fn select_maybe_flavor_quota_by_user_and_group_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    user_id: u64,
+    flavor_group_id: u64,
+) -> Result<Option<FlavorQuota>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT
+            q.id as id,
+            u.id as user,
+            u.name as username,
+            q.quota as quota,
+            g.id as flavor_group,
+            g.name as flavor_group_name
+        FROM
+            quota_flavorquota as f,
+            quota_quota as q,
+            resources_flavorgroup as g,
+            user_user as u
+        WHERE
+            f.quota_ptr_id = q.id AND
+            f.flavor_group_id = g.id AND
+            q.user_id = u.id AND
+            u.id = ? AND
+            g.id = ?
+        "#,
+        user_id,
+        flavor_group_id
+    );
+    let row = transaction
+        .fetch_optional(query)
+        .await
+        .context("Failed to execute select query")?;
+    Ok(match row {
+        Some(row) => Some(
+            FlavorQuota::from_row(&row)
+                .context("Failed to parse flavor quota row")?,
+        ),
+        None => None,
+    })
+}
+
+#[tracing::instrument(
+    name = "select_flavor_quota_by_user_and_group_from_db",
+    skip(transaction)
+)]
+pub async fn select_flavor_quota_by_user_and_group_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    user_id: u64,
+    flavor_group_id: u64,
+) -> Result<FlavorQuota, NotFoundOrUnexpectedApiError> {
+    select_maybe_flavor_quota_by_user_and_group_from_db(
+        transaction,
+        user_id,
+        flavor_group_id,
+    )
+    .await?
+    .ok_or(NotFoundOrUnexpectedApiError::NotFoundError)
+}
+
+#[tracing::instrument(
     name = "insert_flavor_quota_into_db",
     skip(new_flavor_quota, transaction)
 )]
