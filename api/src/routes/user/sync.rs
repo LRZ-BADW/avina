0de9fdbf3222837afd39a6a0c38a292cc0fae512
Sandro-Alessio Gierens <sandro@gierens.de>
2026-01-20 15:37:43 +0100

    feat(api/user): implement user class update in user_sync
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/sync.rs
+++ api/src/routes/user/sync.rs
@@ -3,12 +3,17 @@ use actix_web::{
     web::{Data, ReqData},
 };
 use anyhow::Context;
-use avina_wire::user::{User, UserSync};
+use avina_wire::user::{User, UserClass, UserSync};
 use sqlx::MySqlPool;
 
 use crate::{
     authorization::require_admin_user,
-    database::user::user::{select_all_users_from_db, update_user_role_in_db},
+    database::user::{
+        project::{
+            select_all_projects_from_db, update_project_user_class_in_db,
+        },
+        user::{select_all_users_from_db, update_user_role_in_db},
+    },
     error::NormalApiError,
     ldap::AvinaLdap,
     startup::AvinaLdapConfig,
@@ -43,10 +48,29 @@ pub async fn user_sync(
         }
     }
 
+    let projects = select_all_projects_from_db(&mut transaction).await?;
+    let mut updated_project_count = 0;
+    for project in projects {
+        let user_class = ldap_data.get_userclass(&project.name);
+        if project.user_class == UserClass::NA && user_class != UserClass::NA {
+            update_project_user_class_in_db(
+                &mut transaction,
+                project.id,
+                user_class,
+            )
+            .await
+            .context(format!(
+                "Could not update user class of project {}.",
+                project.name
+            ))?;
+            updated_project_count += 1;
+        }
+    }
+
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .json(UserSync {
-            updated_project_count: 0,
+            updated_project_count,
             updated_user_count,
         }))
 }
