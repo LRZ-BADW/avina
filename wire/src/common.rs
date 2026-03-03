4910ebf61c17f8ddbaa13eb8b7ea9dc9528144c5
Sandro-Alessio Gierens <sandro@gierens.de>
2026-03-03 13:23:16 +0100

    docs(api/common): add todo comment regarding option ref
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- wire/src/common.rs
+++ wire/src/common.rs
@@ -1,5 +1,6 @@
 use std::{borrow::Borrow, fmt::Display};
 
+// TODO: use Option<&T> instead
 pub fn display_option<T: Display>(option: &Option<T>) -> String {
     match option {
         Some(value) => value.to_string(),
