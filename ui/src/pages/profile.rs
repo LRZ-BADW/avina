ef61d1aa8e845e2d3edff2d698186bf31e4f1afa
Sandro-Alessio Gierens <sandro@gierens.de>
2026-04-08 17:55:20 +0200

    feat(ui/profile): add proper error handling for api request
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- ui/src/pages/profile.rs
+++ ui/src/pages/profile.rs
@@ -1,20 +1,32 @@
-use crate::API_URL;
-use avina::{Api, Token};
-use dioxus::prelude::*;
 use std::str::FromStr;
+
+use avina::{Api, Token, error::ApiError};
+use dioxus::prelude::*;
+
+use crate::API_URL;
 
 #[component]
 pub fn ProfilePage(token: String) -> Element {
     let future = use_resource(move || {
         let token_str = token.clone();
         async move {
-            let token = Token::from_str(&token_str).unwrap();
-            let api = Api::new(API_URL.to_string(), token, None, None).unwrap();
-            api.user.me().await.unwrap()
+            let token = Token::from_str(&token_str)?;
+            let api = Api::new(API_URL.to_string(), token, None, None)?;
+            api.user.me().await
         }
     });
-    let Some(user) = future.read_unchecked().as_ref().cloned() else {
-        return rsx! {};
+    let user = match future.read_unchecked().as_ref() {
+        Some(Ok(user)) => user.clone(),
+        Some(Err(ApiError::ResponseError(message))) => {
+            return rsx! { p { b { "Error: " }, "{message}" } };
+        }
+        Some(Err(ApiError::UnexpectedError(_error))) => {
+            // TODO: log the message
+            return rsx! { p { b { "Error: " }, "Internal error, please contact support." } };
+        }
+        None => {
+            return rsx! { p { "Loading ..." } };
+        }
     };
     let role = if user.is_staff {
         "Administrator"
