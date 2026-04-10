66b63437916a1cbda7aa492e7bc47b0d6e7d19c5
Sandro-Alessio Gierens <sandro@gierens.de>
2026-04-10 15:56:21 +0200

    feat(ui): add simple flavor price page, remove hello page
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- ui/src/pages/hello.rs
+++
@@ -1,9 +0,0 @@
-use dioxus::prelude::*;
-
-#[component]
-pub fn HelloPage(api_url: String, token: String) -> Element {
-    let hello = api_call!(api_url, token, api, api.hello.user().await);
-    rsx! {
-        p { "{hello}" }
-    }
-}
