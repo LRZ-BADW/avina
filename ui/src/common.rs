68df63c22affa484a1bc80a6db28b7b3047b9eaf
Sandro-Alessio Gierens <sandro@gierens.de>
2026-04-10 12:46:13 +0200

    feat(ui): improve api_call macro and add return_unexpected_error macro
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- ui/src/common.rs
+++ ui/src/common.rs
@@ -1,31 +1,39 @@
-#[macro_export]
+macro_rules! return_unexpected_error {
+    ($($arg:tt)*) => {
+        tracing::error!($($arg)*);
+        return rsx! { p { b { "Error: " }, "Unexpected error, please contact support." } }
+    };
+}
+
 macro_rules! api_call {
-    ($api_url:expr, $token:expr, $api:ident, $call:expr) => {
+    ($api_url:expr, $token:expr, $api:ident, $call:expr) => {{
+        use std::str::FromStr;
+
+        use avina::{Api, Token, error::ApiError};
+
+        match use_resource(move || {
+            let token_str = $token.clone();
+            let url = $api_url.clone();
+            async move {
+                let token = Token::from_str(&token_str)?;
+                let $api = Api::new(url, token, None, None)?;
+                $call
+            }
+        })
+        .read()
+        .as_ref()
         {
-            use avina::{Api, Token, error::ApiError};
-            use std::str::FromStr;
-            match use_resource(move || {
-                let token_str = $token.clone();
-                let url = $api_url.clone();
-                async move {
-                    let token = Token::from_str(&token_str)?;
-                    let $api = Api::new(url, token, None, None)?;
-                    $call
-                }
-            }).read().as_ref() {
-                Some(Ok(value)) => value.clone(),
-                Some(Err(ApiError::ResponseError(message))) => {
-                    tracing::warn!("API Error Response: {message}");
-                    return rsx! { p { b { "Error: " }, "{message}" } };
-                }
-                Some(Err(ApiError::UnexpectedError(error))) => {
-                    tracing::error!("Unexpected API Error: {error}");
-                    return rsx! { p { b { "Error: " }, "Unexpected error, please contact support." } };
-                }
-                None => {
-                    return rsx! { p { "Loading ..." } };
-                }
+            Some(Ok(value)) => value.clone(),
+            Some(Err(ApiError::ResponseError(message))) => {
+                tracing::warn!("API Error Response: {message}");
+                return rsx! { p { b { "Error: " }, "{message}" } };
+            }
+            Some(Err(ApiError::UnexpectedError(error))) => {
+                return_unexpected_error!("Unexpected API Error: {}", error);
+            }
+            None => {
+                return rsx! { p { "Loading ..." } };
             }
         }
-    }
+    }};
 }
