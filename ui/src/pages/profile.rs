53b08e9215809e43e79f3cdeb5f1abbf8d488309
Sandro-Alessio Gierens <sandro@gierens.de>
2026-04-23 13:17:50 +0200

    feat(ui/profile): add Role enum and From and Display impls
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- ui/src/pages/profile.rs
+++ ui/src/pages/profile.rs
@@ -1,17 +1,46 @@
+use std::fmt::Display;
+
+use avina_wire::user::UserDetailed;
 use dioxus::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Role {
+    Unknown,
+    User,
+    Master,
+    Admin,
+}
+
+impl From<&UserDetailed> for Role {
+    fn from(value: &UserDetailed) -> Self {
+        if value.is_staff {
+            Role::Admin
+        } else if value.role == 2 {
+            Role::Master
+        } else if value.role == 1 {
+            Role::User
+        } else {
+            Role::Unknown
+        }
+    }
+}
+
+impl Display for Role {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Role::Admin => "Administrator",
+            Role::Master => "Master User",
+            Role::User => "User",
+            Role::Unknown => "Unknown",
+        }
+        .fmt(f)
+    }
+}
 
 #[component]
 pub fn ProfilePage(api_url: String, token: String) -> Element {
     let user = api_call!(api_url, token, api, api.user.me().await);
-    let role = if user.is_staff {
-        "Administrator"
-    } else if user.role == 2 {
-        "Master User"
-    } else if user.role == 1 {
-        "User"
-    } else {
-        "Unknown"
-    };
+    let role = Role::from(&user);
     rsx! {
         h2 { "Profile" }
         hr {}
