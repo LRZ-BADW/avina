e4f39df36efded84921f71a02d1e4d0b0e0fd87f
Sandro-Alessio Gierens <sandro@gierens.de>
2026-04-23 13:35:37 +0200

    feat(ui/budget): add sub page bar and empty project and all sub pages
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- ui/src/pages/budget.rs
+++ ui/src/pages/budget.rs
@@ -1,13 +1,114 @@
 use std::collections::HashMap;
 
+use avina_wire::user::UserDetailed;
 use dioxus::prelude::*;
+use strum::{EnumIter, IntoEnumIterator};
 
-use crate::components::charts::*;
+use crate::{
+    components::{button::*, charts::*},
+    pages::profile::Role,
+};
+
+#[derive(Debug, EnumIter, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum SubPage {
+    User,
+    Project,
+    All,
+}
+
+impl SubPage {
+    fn min_role(&self) -> Role {
+        match self {
+            SubPage::All => Role::Admin,
+            SubPage::Project => Role::Master,
+            _ => Role::User,
+        }
+    }
+}
+
+macro_rules! rsx_with_sub_page_bar {
+    ($signal:ident, $role:ident, $page:ty, $content:stmt) => {
+        rsx! {
+            h2 { "Budgets and Costs" }
+            hr {}
+            div {
+                for page in SubPage::iter() {
+                    if page.min_role() <= $role {
+                        Button {
+                            variant: if page == $page {
+                                ButtonVariant::Ghost
+                            } else {
+                                ButtonVariant::Outline
+                            },
+                            disabled: page == $page,
+                            onclick: move |_| *$signal.write() = page,
+                            "{page:?}"
+                        }
+                    }
+                }
+            }
+            br {}
+            div {
+                class: "container-fluid",
+                $content
+            }
+        }
+    };
+}
 
 #[component]
 pub fn BudgetPage(api_url: String, token: String) -> Element {
     let user =
         api_call!(api_url.clone(), token.clone(), api, api.user.me().await);
+    let role = Role::from(&user);
+
+    let mut signal = use_signal(|| SubPage::User);
+    match *signal.read() {
+        SubPage::User => {
+            rsx_with_sub_page_bar!(
+                signal,
+                role,
+                SubPage::User,
+                BudgetUserSubPage {
+                    api_url,
+                    token,
+                    user
+                }
+            )
+        }
+        SubPage::Project => {
+            rsx_with_sub_page_bar!(
+                signal,
+                role,
+                SubPage::Project,
+                BudgetProjectSubPage {
+                    api_url,
+                    token,
+                    user
+                }
+            )
+        }
+        SubPage::All => {
+            rsx_with_sub_page_bar!(
+                signal,
+                role,
+                SubPage::All,
+                BudgetAllSubPage {
+                    api_url,
+                    token,
+                    user
+                }
+            )
+        }
+    }
+}
+
+#[component]
+pub fn BudgetUserSubPage(
+    api_url: String,
+    token: String,
+    user: UserDetailed,
+) -> Element {
     let budget_over_tree = api_call!(
         api_url,
         token,
@@ -36,9 +137,6 @@ pub fn BudgetPage(api_url: String, token: String) -> Element {
         .collect::<HashMap<_, _>>();
 
     rsx! {
-        h2 { "Budgets and Costs" }
-        hr {}
-
         div {
             class: "row",
             h3 { "Budgets" }
@@ -116,5 +214,27 @@ pub fn BudgetPage(api_url: String, token: String) -> Element {
                 }
             }
         }
+    }
+}
+
+#[component]
+pub fn BudgetProjectSubPage(
+    api_url: String,
+    token: String,
+    user: UserDetailed,
+) -> Element {
+    rsx! {
+        p { "TODO" }
+    }
+}
+
+#[component]
+pub fn BudgetAllSubPage(
+    api_url: String,
+    token: String,
+    user: UserDetailed,
+) -> Element {
+    rsx! {
+        p { "TODO" }
     }
 }
