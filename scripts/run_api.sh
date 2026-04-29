4d4887a191d4cd89a239870bacd2c2725e03db7c
Sandro-Alessio Gierens <sandro@gierens.de>
2026-04-29 13:32:41 +0200

    feat(scripts): move config_env.sh into run_api.sh and update docs
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- scripts/run_api.sh
+++ scripts/run_api.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [[ -z $OS_AUTH_URL || -z $OS_USERNAME || -z $OS_PASSWORD || -z $OS_PROJECT_NAME || -z $OS_PROJECT_ID || -z $OS_USER_DOMAIN_NAME || -z $OS_PROJECT_DOMAIN_ID ]]; then
+    echo "Some OpenStack environment variable is missing. Please source the admin-openrc.sh!"
+    exit 1
+fi
+
+export APP_OPENSTACK__KEYSTONE_ENDPOINT="${OS_AUTH_URL}"
+export APP_OPENSTACK__USERNAME="${OS_USERNAME}"
+export APP_OPENSTACK__PASSWORD="${OS_PASSWORD}"
+export APP_OPENSTACK__PROJECT="${OS_PROJECT_NAME}"
+export APP_OPENSTACK__PROJECT_ID="${OS_PROJECT_ID}"
+export APP_OPENSTACK__DOMAIN="${OS_USER_DOMAIN_NAME}"
+export APP_OPENSTACK__DOMAIN_ID="${OS_PROJECT_DOMAIN_ID}"
+
 cargo run \
     --bin avina-api \
     | bunyan
