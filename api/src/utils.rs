1919fa985dd6d1ccf0c756ac15cee3409e3530b2
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-07 20:38:13 +0200

    docs(api/utils): add docstrings for module and types
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/utils.rs
+++ api/src/utils.rs
@@ -1,5 +1,8 @@
+//! Various utility functions.
+
 use chrono::{DateTime, TimeZone, Utc};
 
+/// Wrap the given displayable type inside a bad request error.
 pub fn e400<T>(e: T) -> actix_web::Error
 where
     T: std::fmt::Debug + std::fmt::Display + 'static,
@@ -7,6 +10,7 @@ where
     actix_web::error::ErrorBadRequest(e)
 }
 
+/// Wrap the given displayable type inside an internal server error.
 pub fn e500<T>(e: T) -> actix_web::Error
 where
     T: std::fmt::Debug + std::fmt::Display + 'static,
@@ -14,6 +18,7 @@ where
     actix_web::error::ErrorInternalServerError(e)
 }
 
+/// Get the start of the given year as datetime.
 pub fn start_of_the_year(year: u32) -> DateTime<Utc> {
     // TODO: handle this unwrap
     Utc.with_ymd_and_hms(year as i32, 1, 1, 1, 0, 0).unwrap()
