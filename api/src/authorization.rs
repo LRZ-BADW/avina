92d3d6cdf250c20dc3a46d927f536c046cc808ef
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-07 20:50:10 +0200

    docs(api/authorization): add docstrings to module and functions
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/authorization.rs
+++ api/src/authorization.rs
@@ -1,7 +1,10 @@
+//! Helper functions for checking user authorization.
+
 use avina_wire::user::User;
 
 use crate::error::{AuthOnlyError, NotFoundOnlyError};
 
+/// Return an authorization error if the given user is not an admin.
 pub fn require_admin_user(user: &User) -> Result<(), AuthOnlyError> {
     if !user.is_staff {
         return Err(AuthOnlyError::AuthorizationError(
@@ -11,6 +14,10 @@ pub fn require_admin_user(user: &User) -> Result<(), AuthOnlyError> {
     Ok(())
 }
 
+/// Return an not-found error if the given user is not an admin.
+///
+/// This is supposed to be used instead of [require_admin_user] on endpoints that might return
+/// not-found, to ensure that non-admin users can't even see something exists.
 pub fn require_admin_user_or_return_not_found(
     user: &User,
 ) -> Result<(), NotFoundOnlyError> {
@@ -20,6 +27,7 @@ pub fn require_admin_user_or_return_not_found(
     Ok(())
 }
 
+/// Return an authorization error if the given user is not a master user of the given project.
 pub fn require_master_user(
     user: &User,
     project_id: u32,
@@ -33,6 +41,10 @@ pub fn require_master_user(
     Ok(())
 }
 
+/// Return an not-found error if the given user is not a master user of the given project
+///
+/// This is supposed to be used instead of [require_master_user] on endpoints that might return
+/// not-found, to ensure that non-master users can't even see something exists.
 pub fn require_master_user_or_return_not_found(
     user: &User,
     project_id: u32,
@@ -43,6 +55,7 @@ pub fn require_master_user_or_return_not_found(
     Ok(())
 }
 
+/// Return an authorization error if the given user is not a user of the given project.
 pub fn require_project_user(
     user: &User,
     project_id: u32,
@@ -55,6 +68,10 @@ pub fn require_project_user(
     Ok(())
 }
 
+/// Return an not-found error if the given user is not a user of the given project
+///
+/// This is supposed to be used instead of [require_project_user] on endpoints that might return
+/// not-found, to ensure that non-project users can't even see something exists.
 pub fn require_project_user_or_return_not_found(
     user: &User,
     project_id: u32,
@@ -65,6 +82,11 @@ pub fn require_project_user_or_return_not_found(
     Ok(())
 }
 
+/// Return an not-found error if the given user is neither admin, nor master user of the given
+/// project, nor the user with the given ID itself.
+///
+/// This is useful for the complex but common scenario to return information only to the owning
+/// user, their master user, or admins.
 pub fn require_user_or_project_master_or_not_found(
     user: &User,
     user_id: u32,
