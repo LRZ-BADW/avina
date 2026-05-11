a068589c8e129d010c10682b0247488ae79e9eb7
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-11 15:35:41 +0200

    docs(api/main): add docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,18 @@
+//! Top-level module of the avina-api application.
+//!
+//! It imports everything relevant from the also shipped library,
+//! and just initializes and starts it in [main].
+
 use avina_api::{
     configuration::get_configuration,
     startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
 
+/// Main method and entry point of the avina-api application.
+///
+/// It just initializes the tracing, retrieves the configuration,
+/// and then builds and runs the application.
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let subscriber =
