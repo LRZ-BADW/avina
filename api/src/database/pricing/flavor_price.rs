e364ccc7f9074b29d4b377536785251fad376433
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-13 23:04:21 +0200

    docs(api/database/pricing): add docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/database/pricing/flavor_price.rs
+++ api/src/database/pricing/flavor_price.rs
@@ -1,3 +1,5 @@
+//! Queries for flavor prices.
+
 use std::collections::HashMap;
 
 use anyhow::Context;
@@ -12,18 +14,31 @@ use crate::error::{
     MinimalApiError, NotFoundOrUnexpectedApiError, UnexpectedOnlyError,
 };
 
+/// Representation of a flavor price specifically for communication with the database.
+///
+/// This uses types, that can be directly deserialized from SQL and is then converted
+/// to [FlavorPrice] afterwards.
 #[derive(FromRow)]
 pub struct FlavorPriceRow {
+    /// ID of the flavor price.
     #[sqlx(try_from = "i32")]
     pub id: u32,
+    /// ID of the flavor.
     #[sqlx(try_from = "i32")]
     pub flavor: u32,
+    /// Name of the flavor.
     pub flavor_name: String,
+    /// User class of the price.
     pub user_class: u32,
+    /// Actual price per unit (full year usage of the flavor).
     pub unit_price: f64,
+    /// Timestamp from when the price is valid.
+    ///
+    /// It is valid until the next price for the same flavor and user class takes over.
     pub start_time: DateTime<Utc>,
 }
 
+/// Select a flavor price by the given ID from the database, or return [None].
 #[tracing::instrument(
     name = "select_maybe_flavor_price_from_db",
     skip(transaction)
@@ -74,6 +89,10 @@ pub async fn select_maybe_flavor_price_from_db(
     })
 }
 
+/// Select a flavor price with the given ID from the database, or a "not found" error.
+///
+/// This calls [select_maybe_flavor_price_from_db] and then turns a [None] response into a
+/// [NotFoundOrUnexpectedApiError::NotFoundError].
 #[tracing::instrument(name = "select_flavor_price_from_db", skip(transaction))]
 pub async fn select_flavor_price_from_db(
     transaction: &mut Transaction<'_, MySql>,
@@ -84,6 +103,7 @@ pub async fn select_flavor_price_from_db(
         .ok_or(NotFoundOrUnexpectedApiError::NotFoundError)
 }
 
+/// Select a list of all flavor prices from the database.
 #[tracing::instrument(
     name = "select_all_flavor_prices_from_db",
     skip(transaction)
@@ -134,6 +154,8 @@ pub async fn select_all_flavor_prices_from_db(
     Ok(rows)
 }
 
+/// Select a list of flavor prices that are valid during the period given by begin and end
+/// timestamp.
 #[tracing::instrument(
     name = "select_flavor_prices_for_period_from_db",
     skip(transaction)
@@ -205,6 +227,7 @@ pub async fn select_flavor_prices_for_period_from_db(
     Ok(prices)
 }
 
+/// Select a list of flavor prices with the given user class from the database.
 #[tracing::instrument(
     name = "select_flavor_prices_for_userclass_from_db",
     skip(transaction)
@@ -258,6 +281,8 @@ pub async fn select_flavor_prices_for_userclass_from_db(
     Ok(rows)
 }
 
+/// Select a list of flavor prices for a given user class, that are valid during the period given
+/// by begin and end timestamp.
 #[tracing::instrument(
     name = "select_flavor_prices_for_userclass_and_period_from_db",
     skip(transaction)
@@ -332,16 +357,27 @@ pub async fn select_flavor_prices_for_userclass_and_period_from_db(
     Ok(prices)
 }
 
+/// Simplified representation of data needed to create a new flavor price.
 pub struct NewFlavorPrice {
+    /// ID of the flavor.
     pub flavor_id: u64,
+    /// User class for the price.
     pub user_class: UserClass,
+    /// Actual price per unit (full year usage of the flavor).
     pub unit_price: f64,
+    /// Timestamp from when the price is valid.
+    ///
+    /// It is valid until the next price for the same flavor and user class takes over.
     pub start_time: DateTime<Utc>,
 }
 
 impl TryFrom<FlavorPriceCreateData> for NewFlavorPrice {
     type Error = String;
 
+    /// Transform a [FlavorPriceCreateData] into a [NewFlavorPrice].
+    ///
+    /// More specifically this also replaces not inputted data by defaults, e.g.,
+    /// 0. for the unit price, and now in UTC for the start time.
     fn try_from(data: FlavorPriceCreateData) -> Result<Self, Self::Error> {
         Ok(Self {
             flavor_id: data.flavor as u64,
@@ -355,6 +391,7 @@ impl TryFrom<FlavorPriceCreateData> for NewFlavorPrice {
     }
 }
 
+/// Insert a new flavor price based on the given [NewFlavorPrice] into the database.
 #[tracing::instrument(
     name = "insert_flavor_price_into_db",
     skip(new_flavor_price, transaction)
--- api/src/database/pricing/mod.rs
+++ api/src/database/pricing/mod.rs
@@ -1,1 +1,3 @@
+//! Queries for the pricing module of the API, so the flavor prices.
+
 pub mod flavor_price;
