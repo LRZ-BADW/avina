bc21e941b9b9c9f42a5b9294cb98d12f8c53bf09
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-18 17:22:19 +0200

    docs(api/routes/hello): write docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/hello.rs
+++ api/src/routes/hello.rs
@@ -1,3 +1,5 @@
+//! Hello endpoints for users and admins.
+
 use actix_web::{
     HttpResponse, Scope,
     web::{ReqData, get, scope},
@@ -9,12 +11,19 @@ use avina_wire::{
 
 use crate::{authorization::require_admin_user, error::AuthOnlyError};
 
+/// Routes to the hello-user and hello-admin endpoints.
+///
+///   * `GET /api/hello` => [hello_user] endpoint
+///   * `GET /api/hello/admin` => [hello_admin] endpoint
 pub fn hello_scope() -> Scope {
     scope("/hello")
         .route("", get().to(hello_user))
         .route("/admin", get().to(hello_admin))
 }
 
+/// Implementation of the hello-user endpoint.
+///
+/// This simply returns a personalized hello message to an authenticated user.
 #[tracing::instrument(name = "hello_user")]
 pub async fn hello_user(
     user: ReqData<User>,
@@ -30,6 +39,11 @@ pub async fn hello_user(
         })
 }
 
+/// Implementation of the hello-admin endpoint.
+///
+/// This simply returns a personalized hello message to an admin user.
+/// Other users will get an authorization error, when attempting to access
+/// this endpoint.
 #[tracing::instrument(name = "hello_admin")]
 pub async fn hello_admin(
     user: ReqData<User>,
