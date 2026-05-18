9bfc57537caa09c30ff7519b90e35e338cc78063
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-18 17:11:13 +0200

    docs(api/routes): write module level docstring
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/mod.rs
+++ api/src/routes/mod.rs
@@ -1,3 +1,22 @@
+//! Routes and endpoints of the API.
+//!
+//! The submodules contain `*_scopes` that link up routes to functions
+//! for their endpoints. Usually these are situated in subsubmodules
+//! which contain only this endpoint, e.g., `user_get` in `user::user::get`,
+//! sometimes along with helper functions.
+//!
+//! For resources like a `User` or `FlavorPrice`, there are usually at least
+//! the typical CRUD endpoints:
+//!
+//!   * `POST ./`: create a new instance based of the request data.
+//!   * `GET .`: get a list of instances of this resource. Filters are passed via URL encoding.
+//!   * `GET ./{id}`: get a single instance of this resource by its ID.
+//!   * `PATCH ./{id}/`: modify the instance of the resource with the given ID according to the
+//!     request data.
+//!   * `DELETE ./{id}/`: delete the instance of the resource with the given ID.
+//!
+//! Additional endpoints not following this convention may exist on further routes.
+
 mod accounting;
 mod budgeting;
 mod health_check;
