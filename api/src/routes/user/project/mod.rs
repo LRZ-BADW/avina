c6e887cc4833b3a30b8a83146b87e27dc0672e60
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-18 21:11:10 +0200

    docs(api/routes/project): write docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project/mod.rs
+++ api/src/routes/user/project/mod.rs
@@ -1,3 +1,5 @@
+//! Endpoints for users.
+
 use actix_web::{
     Scope,
     web::{delete, get, patch, post, scope},
@@ -15,6 +17,13 @@ use modify::project_modify;
 pub mod delete;
 use delete::project_delete;
 
+/// Routes to project endpoints.
+///
+///   * `POST /api/user/projects/` => [project_create] endpoint
+///   * `GET /api/user/projects` => [project_list] endpoint
+///   * `GET /api/user/projects/{id}` => [project_get] endpoint
+///   * `PATCH /api/user/projects/{id}/` => [project_modify] endpoint
+///   * `GET /api/user/projects/{id}/` => [project_delete] endpoint
 pub fn projects_scope() -> Scope {
     scope("/projects")
         .route("/", post().to(project_create))
@@ -25,9 +34,13 @@ pub fn projects_scope() -> Scope {
         .route("/{project_id}/", delete().to(project_delete))
 }
 
+/// Wrapper type for the project ID parameter to project endpoints.
+///
+/// As this is handed to endpoints as [actix_web::web::Path], it should to have a distinguishable type.
 // TODO: wouldn't a general IdParam be better?
 #[derive(Deserialize, Debug)]
 pub struct ProjectIdParam {
+    /// The wrapped project ID.
     // TODO: why is this necessary, when this is clearly read in query_as
     #[allow(unused)]
     project_id: u32,
