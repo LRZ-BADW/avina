130b5d3fd9f2a48d55bd4c5f62a020ef0289180b
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-20 13:54:57 +0200

    docs(api/routes/resources/flavor_group): write docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/resources/flavor_group/mod.rs
+++ api/src/routes/resources/flavor_group/mod.rs
@@ -1,3 +1,5 @@
+//! Endpoints for flavor groups.
+
 use actix_web::{
     Scope,
     web::{delete, get, patch, post, scope},
@@ -17,6 +19,14 @@ use delete::flavor_group_delete;
 pub mod usage;
 use usage::flavor_group_usage;
 
+/// Routes to flavor group endpoints.
+///
+///   - `POST /api/resources/flavorgroups/` => [flavor_group_create] endpoint
+///   - `GET /api/resources/flavorgroups` => [flavor_group_list] endpoint
+///   - `GET /api/resources/flavorgroups/{id}` => [flavor_group_get] endpoint
+///   - `PATCH /api/resources/flavorgroups/{id}/` => [flavor_group_modify] endpoint
+///   - `GET /api/resources/flavorgroups/{id}/` => [flavor_group_delete] endpoint
+///   - `GET /api/resources/flavorgroups/usage/` => [flavor_group_usage] endpoint
 pub fn flavor_groups_scope() -> Scope {
     scope("/flavorgroups")
         .route("/", post().to(flavor_group_create))
@@ -28,9 +38,13 @@ pub fn flavor_groups_scope() -> Scope {
         .route("/usage/", get().to(flavor_group_usage))
 }
 
+/// Wrapper type for the flavor group ID parameter to user endpoints.
+///
+/// As this is handed to endpoints as [actix_web::web::Path], it should to have a distinguishable type.
 // TODO: wouldn't a general IdParam be better?
 #[derive(Deserialize, Debug)]
-struct FlavorGroupIdParam {
+pub struct FlavorGroupIdParam {
+    /// The wrapped flavor group ID.
     // TODO: why is this necessary, when this is clearly read in query_as
     #[allow(unused)]
     flavor_group_id: u32,
