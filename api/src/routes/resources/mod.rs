3b24907535a51f96c89208e6c0fa93c304360646
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-20 11:31:07 +0200

    docs(api/routes/resources): write docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/resources/mod.rs
+++ api/src/routes/resources/mod.rs
@@ -1,3 +1,5 @@
+//! Endpoints for resources, e.g. flavors, flavor groups, and cloudusage.
+
 use actix_web::{Scope, web::scope};
 
 pub mod flavor_group;
@@ -7,6 +9,11 @@ use flavor::flavors_scope;
 mod usage;
 use usage::usage_scope;
 
+/// Routes to resource endpoints:
+///
+///   - `GET /api/resources/flavorgroups` => [flavor_groups_scope], see [flavor_group] submodule
+///   - `GET /api/resources/flavors` => [flavors_scope], see [flavor] submodule
+///   - `GET /api/resources/usage` => [usage_scope], e.g. the cloudusage endpoint, see [usage]
 pub fn resources_scope() -> Scope {
     scope("/resources")
         .service(flavor_groups_scope())
