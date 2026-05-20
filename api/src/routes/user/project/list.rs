e7eff01d7b4d6ae6384a03748eae48629302a96a
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-20 11:12:53 +0200

    docs(api/routes/project/list): write docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project/list.rs
+++ api/src/routes/user/project/list.rs
@@ -1,3 +1,5 @@
+//! Implementation of the project-list endpoint.
+
 use actix_web::{
     HttpResponse,
     web::{Data, Query, ReqData},
@@ -15,6 +17,18 @@ use crate::{
     error::NormalApiError,
 };
 
+/// Get a list of projects.
+///
+/// On success an HTTP 200 OK with the requested list in the response data is returned.
+///
+/// By default this only returns the project of the calling user in a list. To retrieve more, admin
+/// users may use the following filters:
+///
+///   - `all`: returns all projects in the system.
+///   - `user_class`: returns all projects of the given user class.
+///
+/// Note, that given both filters, `all` takes precedence. If users other than admins use any of
+/// them, an [OptionApiError::AuthorizationError] is returned.
 #[tracing::instrument(name = "project_list")]
 pub async fn project_list(
     user: ReqData<User>,
