56b7606e9f92bbbb5a5d57742bb8a8f4eebcd47b
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-21 13:46:18 +0200

    docs(lib/lib): write docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,44 @@
+//! Bindings for the [avina-api].
+//!
+//! The main types of this crate are [Token] which represents a OpenStack Keystone token, and [Api]
+//! which can be created with it, and represents a client to the [avina-api]. [Api] contains
+//! further `...Api` structs for all the respective modules of the API, e.g. [UserApi] in the
+//! [user](Api::user) field.
+//!
+//! Those structs have member functions to call individual endpoints, e.g. [UserApi::get]. Endpoint
+//! bindings that take optional arguments use the [builder pattern]. This allows mnemonic calls
+//! like `api.user.list().all().send().await`. The calls return datatypes from the [avina_wire]
+//! crate.
+//!
+//! # Usage
+//!
+//! Include the following in for `Cargo.toml`:
+//!
+//! ```toml
+//! avina = 2
+//! ```
+//!
+//! Then you can use the bindings like this:
+//!
+//! ```rust
+//! use avina::{Token, Api};
+//!
+//! // let token = Token::from_str("abcdefg...").unwrap();
+//! let token = Token::new(
+//!                 auth_url.as_str(),
+//!                 username.as_str(),
+//!                 password.as_str(),
+//!                 project_name.as_str(),
+//!                 user_domain_name.as_str(),
+//!                 project_domain_id.as_str(),
+//!             ).unwrap();
+//! let api = Api::new("https://cc.lrz.de:1338/api", token, None, None).unwrap();
+//! println!("{:?}", api.user.me());
+//! ```
+//!
+//! [avina-api]: https://docs.rs/avina-api
+//! [builder pattern]: https://rust-unofficial.github.io/patterns/patterns/creational/builder.html
+
 use std::{rc::Rc, time::Duration};
 
 use anyhow::Context;
@@ -59,47 +100,69 @@ use user::ProjectApi;
 #[cfg(feature = "user")]
 use user::UserApi;
 
+/// Default timeout of HTTP calls in seconds (5 minutes)
 pub const DEFAULT_TIMEOUT: u64 = 300;
 
+/// Main API client.
+///
+/// This is a collection of all the individual API module clients.
 #[derive(Debug)]
 pub struct Api {
-    // url: Rc<str>,
+    /// Authentication token.
     #[allow(unused)]
     token: Token,
-    // client: Rc<Client>,
+    /// Client for hello endpoints.
     #[cfg(feature = "hello")]
     pub hello: HelloApi,
+    /// Client for project endpoints.
     #[cfg(feature = "user")]
     pub project: ProjectApi,
+    /// Client for user endpoints.
     #[cfg(feature = "user")]
     pub user: UserApi,
+    /// Client for flavor endpoints.
     #[cfg(feature = "resources")]
     pub flavor: FlavorApi,
+    /// Client for flavor group endpoints.
     #[cfg(feature = "resources")]
     pub flavor_group: FlavorGroupApi,
+    /// Client for the usage endpoint.
     #[cfg(feature = "resources")]
     pub usage: UsageApi,
+    /// Client for flavor price endpoints.
     #[cfg(feature = "pricing")]
     pub flavor_price: FlavorPriceApi,
+    /// Client for flavor quota endpoints.
     #[cfg(feature = "quota")]
     pub flavor_quota: FlavorQuotaApi,
+    /// Client for server state endpoints.
     #[cfg(feature = "accounting")]
     pub server_state: ServerStateApi,
+    /// Client for server cost endpoint.
     #[cfg(feature = "accounting")]
     pub server_cost: ServerCostApi,
+    /// Client for server consumption endpoints.
     #[cfg(feature = "accounting")]
     pub server_consumption: ServerConsumptionApi,
+    /// Client for project budget endpoints.
     #[cfg(feature = "budgeting")]
     pub project_budget: ProjectBudgetApi,
+    /// Client for user budget endpoints.
     #[cfg(feature = "budgeting")]
     pub user_budget: UserBudgetApi,
+    /// Client for the budget-over-tree endpoint.
     #[cfg(feature = "budgeting")]
     pub budget_over_tree: BudgetOverTreeApi,
+    /// Client for the budget-bulk-create endpoint.
     #[cfg(feature = "budgeting")]
     pub budget_bulk_create: BudgetBulkCreateApi,
 }
 
 impl Api {
+    /// Create a new API client for the given API URL and with the given authentication token.
+    ///
+    /// Optionally, the request timeout and a user ID for impersonation may be configured.
+    /// The latter is only allowed for admin users.
     pub fn new(
         // TODO: this should be a url::Url
         url: String,
