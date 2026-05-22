4b1dc08fb862d63c672a0212952e08f180a3b409
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-22 22:44:42 +0200

    docs(lib/common): write docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- lib/src/common.rs
+++ lib/src/common.rs
@@ -1,3 +1,5 @@
+//! Common helper types and functions.
+
 use std::fmt::Debug;
 
 use anyhow::Context;
@@ -7,8 +9,18 @@ use serde::{de::DeserializeOwned, ser::Serialize};
 
 use crate::error::ApiError;
 
+/// Zero-size marker type that implements [serde::Serialize].
+///
+/// This is use by the [SerializableNone] macro to pass a [None] as parameter that only requires
+/// a generic implementing [serde::Serialize] without needing type annotations.
 #[derive(serde::Serialize, Debug)]
-pub(crate) struct SerializableFoo {}
+pub(crate) struct SerializableFoo;
+
+/// Returns a [None] with an annotation of a serializable marker type.
+///
+/// This can be passed as argument for generic function parameters, those requirement is
+/// that they implement [serde::Serialize], without having to annotate a type. This is
+/// helpful for the `data` parameter of the [request_bare] and [request] functions.
 macro_rules! SerializableNone {
     () => {
         None::<crate::common::SerializableFoo>
@@ -16,6 +28,26 @@ macro_rules! SerializableNone {
 }
 pub(crate) use SerializableNone;
 
+/// Perform an HTTP request and return the bare response.
+///
+/// This function is generic over the request data, those only requirement is,
+/// that is implements [Serialize] and [Debug]. It sends the data with the
+/// given HTTP method to the given URL, using the given client. It matches
+/// the returned response status code against the given one and otherwise
+/// returns an [ApiError], but otherwise just returns the received [Response]
+///
+/// When the API returned an error message, this is wrapped in an
+/// [ApiError::ResponseError], otherwise or if something on the client side
+/// went wrong, like serializing request data or sending the request, the
+/// [ApiError::UnexpectedError] is returned.
+///
+/// # Arguments
+///
+///   - `client` - HTTP client.
+///   - `method` - Request method to use.
+///   - `url` - URL to call.
+///   - `data` - Generic serializable request data to send.
+///   - `expected_status` - Response status code to expect.
 pub(crate) async fn request_bare<T>(
     client: &Client,
     method: Method,
@@ -54,6 +86,19 @@ where
     Ok(response)
 }
 
+/// Perform an HTTP request and deserialize the response data.
+///
+/// This calls [request_bare] and attempts to deserialize the response data
+/// into the second generic type, that needs to implement [DeserializeOwned].
+/// If this fails it returns an [ApiError::UnexpectedError].
+///
+/// # Arguments
+///
+///   - `client` - HTTP client.
+///   - `method` - Request method to use.
+///   - `url` - URL to call.
+///   - `data` - Generic serializable request data to send.
+///   - `expected_status` - Response status code to expect.
 pub(crate) async fn request<T, U>(
     client: &Client,
     method: Method,
