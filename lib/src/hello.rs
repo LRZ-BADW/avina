b275575a192a58eaaeb0e0a2b675e88eb927c2c3
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-22 22:56:15 +0200

    docs(lib/hello): write docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- lib/src/hello.rs
+++ lib/src/hello.rs
@@ -1,3 +1,5 @@
+//! Bindings for hello endpoints of the API.
+
 use std::rc::Rc;
 
 use avina_wire::hello::Hello;
@@ -8,13 +10,17 @@ use crate::{
     error::ApiError,
 };
 
+/// Client for the hello module of the API.
 #[derive(Debug)]
 pub struct HelloApi {
+    /// URL of the hello module of the API.
     pub url: String,
+    /// Reference to the HTTP client.
     pub client: Rc<Client>,
 }
 
 impl HelloApi {
+    /// Build a new instance from the given base URL and HTTP client reference.
     pub fn new(base_url: &str, client: &Rc<Client>) -> HelloApi {
         HelloApi {
             url: format!("{base_url}/hello"),
@@ -22,6 +28,7 @@ impl HelloApi {
         }
     }
 
+    /// Get a hello message for admin users.
     pub async fn admin(&self) -> Result<Hello, ApiError> {
         request(
             &self.client,
@@ -33,6 +40,7 @@ impl HelloApi {
         .await
     }
 
+    /// Get a hello message for normal users.
     pub async fn user(&self) -> Result<Hello, ApiError> {
         request(
             &self.client,
