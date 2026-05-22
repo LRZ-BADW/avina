8699cf8308d016134ab1b8e26aa62f54a7310ea3
Sandro-Alessio Gierens <sandro@gierens.de>
2026-05-22 23:52:41 +0200

    docs(api/routes/accounting): add docstrings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/accounting/mod.rs
+++ api/src/routes/accounting/mod.rs
@@ -1,3 +1,5 @@
+//! Endpoints for server status, consumption and cost.
+
 use actix_web::{Scope, web::scope};
 
 pub mod server_state;
@@ -7,6 +9,12 @@ use server_consumption::server_consumption_scope;
 pub mod server_cost;
 use server_cost::server_cost_scope;
 
+/// Main scope for routes server state, consumption and cost endpoints.
+///
+///   - `/api/accounting/serverstates` => [server_states_scope], see [server_state] submodule
+///   - `/api/accounting/serverconsumption` => [server_consumption_scope], see
+///     [server_consumption] submodule
+///   - `/api/accounting/servercost` => [server_cost_scope], see [server_cost] submodule
 pub fn accounting_scope() -> Scope {
     scope("/accounting")
         .service(server_states_scope())
